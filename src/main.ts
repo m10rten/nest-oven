e42ec694cbd0658d5c6685a4730cec526dfabe55
m10rten <qmaartenenzo@gmail.com>
2024-07-09 16:24:10 +0200

    add swagger basic

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('The NestJS API description')
+    .setVersion('1.0')
+    .addTag('nestjs')
+    .build();
+  const ducument = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('schema', app, ducument);
+
   await app.listen(3000);
 }
 bootstrap();
